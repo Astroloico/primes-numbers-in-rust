582c082ba940460b4af8c44a3c539d2175bd8a7b
admin <loic.duhamel10@gmail.com>
2024-09-24 22:19:52 +0000

    created main loop + io improvements

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::str;
 use std::io::*;
+use std::vec;
+use std::f32;
 
 fn main() {
 	let mut input:String = String::new();
 	stdin().read_line(&mut input).unwrap();
 	input = input.trim().parse().unwrap();
 	println!("You typed: {}", input.trim());
-
-	let n:i64 = str::parse::<i64>(&input).unwrap_or_else(|e| {
-		panic!("{}", e);
-	});
-	println!("You typed: {}", n);
+	let n:usize = str::parse::<usize>(&input).unwrap_or_else(|e| {
+		panic!("{}", e); });
+	let mut bool_vec:Vec<bool> = vec![true; n];
+	let n_sqrt:usize = f32::sqrt(n as f32).ceil() as usize;
+	for i in 0..n_sqrt {
+		print!("{} ", bool_vec[i]);
+	}
 }
