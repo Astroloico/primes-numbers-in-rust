bfec5c5d85e34eb784fd3dd968838e69467cc243
admin <loic.duhamel10@gmail.com>
2024-09-24 21:52:40 +0000

    got i/o working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::str;
+use std::io::*;
+
 fn main() {
-    println!("Hello, world!");
+	let mut input:String = String::new();
+	stdin().read_line(&mut input).unwrap();
+	input = input.trim().parse().unwrap();
+	println!("You typed: {}", input.trim());
+
+	let n:i64 = str::parse::<i64>(&input).unwrap_or_else(|e| {
+		panic!("{}", e);
+	});
+	println!("You typed: {}", n);
 }
