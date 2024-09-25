41fbe5e8eabc5cc429c531072a1ffdc07a27986b
admin <loic.duhamel10@gmail.com>
2024-09-25 00:16:41 +0000

    stuff

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,22 @@ fn main() {
 	let mut input:String = String::new();
 	stdin().read_line(&mut input).unwrap();
 	input = input.trim().parse().unwrap();
-	println!("You typed: {}", input.trim());
 	let n:usize = str::parse::<usize>(&input).unwrap_or_else(|e| {
 		panic!("{}", e); });
 	let mut bool_vec:Vec<bool> = vec![true; n];
 	let n_sqrt:usize = f32::sqrt(n as f32).ceil() as usize;
-	for i in 0..n_sqrt {
-		print!("{} ", bool_vec[i]);
+	for i in 2..n_sqrt + 1 {
+		if bool_vec[i] {
+			print!("{} ", i);
+			for j in i*i..n {
+				if j % i == 0 {
+					bool_vec[j] = false;
+				}
+			}
+		}
+	} for i in n_sqrt + 1..n {
+		if bool_vec[i] {
+			print!("{} ", i);
+		}
 	}
 }
